f4e3d62d264e4a1737da0a6fd5341e052404c60a
Stachacl <stacha.l@icloud.com>
2023-02-17 14:33:30 +1100

    chart styles applied

--- main.js
+++ main.js
@@ -4,16 +4,22 @@ $(document).ready(function() {
 
 const ctx = document.getElementById('analyticsChart').getContext('2d');
 
+//Gradient Fill
+let gradient = ctx.createLinearGradient(0, 0, 0, 400);
+gradient.addColorStop(0, 'rgba(54, 162, 235, 0.9)');
+gradient.addColorStop(1, 'rgba(255, 162, 207, 0.05)');
+
 
 // Define chart data
 const chartData = {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     datasets: [{
       label: 'Revenue',
-      data: [15000, 20000, 18000, 22000, 25000, 28000, 30000, 32000, 34000, 36000, 38000, 40000],
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      data: [1500, 2000, 1800, 2200, 2800, 3200, 2900, 2500, 3400, 3600, 3800, 4000],
+      backgroundColor: gradient,
       borderColor: 'rgba(54, 162, 235, 1)',
-      borderWidth: 1
+      borderWidth: 1,
+      fill: true,
     }]
   };
   
@@ -26,6 +32,15 @@ const chartData = {
     data: chartData,
     options: {
       responsive: true,
+      scales: {
+        y: {
+            ticks: {
+                callback: function(value){
+                    return 'AUD ' + value + "m"
+                }
+            }
+        }
+      }
     }
   }
 
