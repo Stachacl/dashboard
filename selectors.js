f45f3217db58cf89f360e345633c7ca4e3a5976c
Stachacl <stacha.l@icloud.com>
2023-02-19 09:06:44 +1100

    on click change added and hover effect to the contact block

--- selectors.js
+++ selectors.js
@@ -2,14 +2,25 @@
 
 $("document").ready(function () {
 
-  $("body").append('<div class="px-3"> <h4> Contact us now! </h4> </div>');
+  $("#clickBox").on("click", whenMouseIsClicked);
+  $("#clickBox").on("mouseleave", whenMouseLeaves);
 
-  $("p:first").css("border","1px solid grey")
-//   $("p:last").css("border","1px solid blue")
+  function whenMouseIsClicked(){
+    $("#clickBox").html("<h5> Thank you! </h5>");
 
+  }
 
-    var newText = $("<p>");
-    newText.append("<h5> Please note that this is <i> a mockup version <i> of the dashboard and the information provided is not real. We hope you find this helpful in visualizing the potential functionality of our product. </h5>");
-    $("#textBox").html(newText);
+  function whenMouseLeaves(){
+    $("#clickBox").html("<h5> See you soon! </h5>");
+    
+  }
 
+  //   $("body").append('<div class="px-3"> <h4> Contact us now! </h4> </div>');
+
+  //   $("p:first").css("border","1px solid grey")
+  // //   $("p:last").css("border","1px solid blue")
+
+  //     var newText = $("<p>");
+  //     newText.append("<h5>  Our platform is designed to provide you with a comprehensive view of your financial health, all in one easy-to-use location. Whether you're a seasoned investor or just starting out, our dashboard is tailored to meet your unique needs. </h5>");
+  //     $("#textBox").html(newText);
 });
