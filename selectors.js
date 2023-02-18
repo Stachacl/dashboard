d121b4e853e7b52638a002218b01d6f15834b0e6
Stachacl <stacha.l@icloud.com>
2023-02-18 13:48:16 +1100

    added styles with jQuery

--- selectors.js
+++ selectors.js
@@ -1,5 +1,10 @@
 //Query course
 
 $("document").ready(function () {
+
   $("body").append('<div class="px-3"> <h4> Message </h4> </div>');
+
+  $("p:first").css("border","1px solid blue")
+  $("p:last").css("border","1px solid grey")
+
 });
